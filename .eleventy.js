5da357f9ab2a782607e77a1a551eaa7fa87fb013
Josh Buchea <verctrl@joshbuchea.com>
2020-06-06 20:45:36 -0700

    update path prefix

--- .eleventy.js
+++ .eleventy.js
@@ -75,7 +75,7 @@ module.exports = function(eleventyConfig) {
     // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
 
     // You can also pass this in on the command line using `--pathprefix`
-    pathPrefix: "/resumes-site",
+    pathPrefix: "/resume",
 
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
